499a782da88dd6432831416804f3794af194c42a
justMaxIt <maximfedor2015@gmail.com>
2020-09-24 20:09:40 +0300

    11 lesson add grid blocks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://blog.matcharesident.com/wp-content/uploads/2019/07/iStock-944453634-750x450.jpg"></img>
       </header>
+      <nav className="nav">
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img src="https://lh3.googleusercontent.com/proxy/wBSbX6PluWaLUubAqHY7PGu5LIhRhkiGeLumggi6H12YEWB4AkcgLgyDhIwAK4CVBGQSQryrw4VBd4yZxBhyos4BHMQQkDitZ0syNZERIZD6gMed9tyAiwKFKoy2_Q8psR6owwHv"></img>
+        </div>
+        <div>ava + description</div>
+        <div>
+          My posts
+          <div>New post</div>
+          <div>
+            <div>post1</div>
+            <div>post2</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
