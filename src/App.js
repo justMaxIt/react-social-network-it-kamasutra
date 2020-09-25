0a0499d5f035b8695cd50f8e58c1937084e40e00
justMaxIt <maximfedor2015@gmail.com>
2020-09-25 17:15:54 +0300

    14 css modules

--- src/App.js
+++ src/App.js
@@ -1,43 +1,15 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 const App = () => {
   return (
     <div className="app-wrapper">
-      <header className="header">
-        <img src="https://blog.matcharesident.com/wp-content/uploads/2019/07/iStock-944453634-750x450.jpg"></img>
-      </header>
-      <nav className="nav">
-        <div>
-          <a>Profile</a>
-        </div>
-        <div>
-          <a>Messages</a>
-        </div>
-        <div>
-          <a>News</a>
-        </div>
-        <div>
-          <a>Music</a>
-        </div>
-        <div>
-          <a>Settings</a>
-        </div>
-      </nav>
-      <div className="content">
-        <div>
-          <img src="https://lh3.googleusercontent.com/proxy/wBSbX6PluWaLUubAqHY7PGu5LIhRhkiGeLumggi6H12YEWB4AkcgLgyDhIwAK4CVBGQSQryrw4VBd4yZxBhyos4BHMQQkDitZ0syNZERIZD6gMed9tyAiwKFKoy2_Q8psR6owwHv"></img>
-        </div>
-        <div>ava + description</div>
-        <div>
-          My posts
-          <div>New post</div>
-          <div>
-            <div>post1</div>
-            <div>post2</div>
-          </div>
-        </div>
-      </div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 };
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import s from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={s.header}>
+      <img src="https://blog.matcharesident.com/wp-content/uploads/2019/07/iStock-944453634-750x450.jpg"></img>
+    </header>
+  );
+};
+export default Header;
