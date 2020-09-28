eab63d87184116a9ca8d1c87aeaa5bf9ce5e567c
justMaxIt <maximfedor2015@gmail.com>
2020-09-28 11:13:29 +0300

    21 machine Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,6 +2,21 @@ import React from "react";
 import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + " " + s.active}>Max</div>
+        <div className={s.dialog}>Vova</div>
+        <div className={s.dialog}>Denis</div>
+        <div className={s.dialog}>Sveta</div>
+        <div className={s.dialog}>Tanya</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>friend</div>
+        <div className={s.message}>yo</div>
+      </div>
+    </div>
+  );
 };
 export default Dialogs;
