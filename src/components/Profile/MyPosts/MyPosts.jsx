1dcd558975907a1f04f0d720b35bfa75d625c486
justMaxIt <maximfedor2015@gmail.com>
2020-10-02 21:29:46 +0300

    data from MyPosts to Profile component solo

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
-const MyPosts = () => {
-  let posts = [
-    { id: "1", message: "Hi, how are you?", likesCount: "15" },
-    { id: "2", message: "It's ok", likesCount: "20" },
-  ];
-  let postsElements = posts.map((p) => (
-    <Post message={p.message} likesCount={p.likesCount} />
-  ));
+// let { posts } = props;
+const MyPosts = (props) => {
+  // let posts = [
+  //   { id: "1", message: "Hi, how are you?", likesCount: "15" },
+  //   { id: "2", message: "It's ok", likesCount: "20" },
+  // ];
+
+  // let postsElements = [posts].map((p) => (
+  //   <Post message={p.message} likesCount={p.likesCount} />
+  // ));
   return (
     <div className={s.postsBlock}>
       <h3>My posts</h3>
@@ -22,7 +24,7 @@ const MyPosts = () => {
         </div>
         {/* <button>Remove</button> */}
       </div>
-      <div className={s.posts}>{postsElements}</div>
+      <div className={s.posts}>{props.postsElements}</div>
     </div>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import MyPosts from "./MyPosts/MyPosts";
 import s from "./Profile.module.css";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-
+import Post from "./MyPosts/Post/Post";
 const Profile = () => {
+  let posts = [
+    { id: "1", message: "Hi, how are you?", likesCount: "15" },
+    { id: "2", message: "It's ok", likesCount: "20" },
+  ];
+  let postsElements = posts.map((p) => (
+    <Post message={p.message} likesCount={p.likesCount} />
+  ));
   return (
     <div className={s.content}>
       <ProfileInfo />
-      <MyPosts />
+      <div>
+        <MyPosts postsElements={postsElements} />
+      </div>
     </div>
   );
 };
