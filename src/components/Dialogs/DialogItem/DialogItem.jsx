d6e2487313a17505ef51b616db0307ba2bcc6438
justMaxIt <maximfedor2015@gmail.com>
2020-10-04 10:45:45 +0300

    add img to dialogs

--- src/components/Dialogs/DialogItem/DialogItem.jsx
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -5,7 +5,8 @@ import s from "./../Dialogs.module.css";
 const DialogItem = (props) => {
   let path = "/dialogs/" + props.id;
   return (
-    <div className={s.dialog + " " + s.active}>
+    <div className={s.dialog}>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRw94OjqMOjukkF1EOn9cO3VUske1EwoWfkkA&usqp=CAU" />
       <NavLink to={path}>{props.name}</NavLink>
     </div>
   );
