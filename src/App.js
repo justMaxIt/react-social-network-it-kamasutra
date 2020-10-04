dce7a4a0c36cefa4412e8b6ac712296ba40bb9b1
justMaxIt <maximfedor2015@gmail.com>
2020-10-04 20:28:03 +0300

    30 BrowserRouter to Index.js

--- src/App.js
+++ src/App.js
@@ -4,30 +4,28 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 const App = (props) => {
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <Header />
-        <Navbar sidebar={props.state.sidebar} />
+    <div className="app-wrapper">
+      <Header />
+      <Navbar sidebar={props.state.sidebar} />
 
-        <div className="app-wrapper-content">
-          <Route
-            path="/profile"
-            render={() => <Profile state={props.state.profilePage} />}
-          />
-          <Route
-            path="/dialogs"
-            render={() => <Dialogs state={props.state.dialogsPage} />}
-          />
-          <Route path="/news" />
-          <Route path="/music" />
-          <Route path="/settings" />
-        </div>
+      <div className="app-wrapper-content">
+        <Route
+          path="/profile"
+          render={() => <Profile state={props.state.profilePage} />}
+        />
+        <Route
+          path="/dialogs"
+          render={() => <Dialogs state={props.state.dialogsPage} />}
+        />
+        <Route path="/news" />
+        <Route path="/music" />
+        <Route path="/settings" />
       </div>
-    </BrowserRouter>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import state from "./redux/state";
+import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
-  <React.StrictMode>
-    <App state={state} />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App state={state} />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
