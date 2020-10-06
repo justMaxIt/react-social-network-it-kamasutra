6b5e371c77b06423d3d6398f4dfe0b1c90b14c63
justMaxIt <maximfedor2015@gmail.com>
2020-10-06 17:26:27 +0300

    33 add render.js FLUX

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 import state from "./redux/state";
-import { addPost } from "./redux/state";
-import { BrowserRouter } from "react-router-dom";
+import { rerenderEntireTree } from "./render";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App state={state} addPost={addPost} />
-    </React.StrictMode>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+rerenderEntireTree(state);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
