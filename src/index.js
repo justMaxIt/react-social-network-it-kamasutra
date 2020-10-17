f9be22318d8cfe1c24bd92a50e2aeca0c1b6a5c2
justMaxIt <maximfedor2015@gmail.com>
2020-10-17 20:46:17 +0300

    47 copy state data at Reducer

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import store from "./redux/reduxStore";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
-let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <BrowserRouter>
       <Provider store={store}>
@@ -17,14 +16,8 @@ let rerenderEntireTree = (state) => {
         </Provider>
     </BrowserRouter>,
     document.getElementById("root")
-  );
-};
-rerenderEntireTree(store.getState());
+  )
 
-
-store.subscribe(() => {
-  let state = store.getState()
-  rerenderEntireTree(state)})
 
 
 serviceWorker.unregister();
