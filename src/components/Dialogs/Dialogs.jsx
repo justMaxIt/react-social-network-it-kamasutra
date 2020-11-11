3b0ac989ccc5ac7caaf30974f9b3f5dceb001345
justMaxIt <maximfedor2015@gmail.com>
2020-11-11 19:07:21 +0200

    48 dialogs reducer deep copy refactoring

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,11 +7,11 @@ import Message from "./Message/Message";
 const Dialogs = (props) => {
 let state= props.dialogsPage
   let dialogsElements = state.dialogs.map((d) => (
-    <DialogItem name={d.name} id={d.id} />
+    <DialogItem name={d.name} key={d.id} id={d.id} />
   ));
 
   let messagesElements = state.messages.map((m) => (
-    <Message message={m.message} />
+    <Message message={m.message} key={m.id}/>
   ));
  
   let addMes = () => {
--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -1,49 +1,52 @@
-const ADD_MES = "ADD-MES"
-const UPDATE_NEW_MES = "UPDATE-NEW-MES"
+const ADD_MES = "ADD-MES";
+const UPDATE_NEW_MES = "UPDATE-NEW-MES";
 
 let initialState = {
-dialogs: [
-        { id: "1", name: "Max" },
-        { id: "2", name: "Vova" },
-        { id: "3", name: "Denis" },
-        { id: "4", name: "Sveta" },
-        { id: "5", name: "Tanya" },
-      ],
-      messages: [
-        { id: "1", message: "Hi" },
-        { id: "2", message: "friend" },
-        { id: "3", message: "yo" },
-        { id: "4", message: "yo" },
-        { id: "5", message: "yo" },
-      ],
-      newTextMes: ""
-}
+  dialogs: [
+    { id: "1", name: "Max" },
+    { id: "2", name: "Vova" },
+    { id: "3", name: "Denis" },
+    { id: "4", name: "Sveta" },
+    { id: "5", name: "Tanya" },
+  ],
+  messages: [
+    { id: "1", message: "Hi" },
+    { id: "2", message: "friend" },
+    { id: "3", message: "yo" },
+    { id: "4", message: "yo" },
+    { id: "5", message: "yo" },
+  ],
+  newTextMes: "",
+};
 
 const dialogsReducer = (state = initialState, action) => {
-    
-    switch (action.type) {
-      case ADD_MES: {
-        let newMes = {
-          id: 6,
-          message: state.newTextMes,
-        };
-        let stateCopy = { ...state };
-        stateCopy.messages.push(newMes);
-        stateCopy.newTextMes = "";
-        return stateCopy;
-      }
-      case UPDATE_NEW_MES: {
-        let stateCopy = { ...state };
-        stateCopy.newTextMes = action.text;
-        return stateCopy;
-      }
-        default: return state
+  switch (action.type) {
+    case UPDATE_NEW_MES: {
+      return {
+        ...state,
+        newTextMes: action.text,
+      };
     }
-}
+    case ADD_MES: {
+      let newMes = {
+        id: 6,
+        message: state.newTextMes,
+      };
+      return {
+        ...state,
+        newTextMes: "",
+        messages: [...state.messages, newMes],
+      };
+    }
+    default:
+      return state;
+  }
+};
 
 export const addMesActionCreator = () => ({ type: ADD_MES });
-export const updateNewMesActionCreator = (text) =>
-  ({type: UPDATE_NEW_MES, text: text})  
+export const updateNewMesActionCreator = (text) => ({
+  type: UPDATE_NEW_MES,
+  text: text,
+});
 
-
-export default dialogsReducer
+export default dialogsReducer;
