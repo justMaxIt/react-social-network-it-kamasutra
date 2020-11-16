d0aa0cdad65b8dc3dc007addd8bfc1c12ec7262e
justMaxIt <maximfedor2015@gmail.com>
2020-11-16 16:19:49 +0200

    50 + add Navlink Users

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -28,6 +28,11 @@ const Navbar = (props) => {
             Music
           </NavLink>
         </div>
+         <div className={s.item}>
+          <NavLink to="/users" activeClassName={s.activeLink}>
+           Users
+          </NavLink>
+        </div>
         <div className={s.item}>
           <NavLink to="/settings" activeClassName={s.activeLink}>
             Settings
