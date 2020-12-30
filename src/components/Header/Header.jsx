27c98eed25ae11a0632ee1483dd2163037c3c000
justMaxIt <maximfedor2015@gmail.com>
2020-12-30 23:58:44 +0200

    61 add login to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from "./Header.module.css";
 
 const Header = () => {
   return (
     <header className={s.header}>
       <img src="https://blog.matcharesident.com/wp-content/uploads/2019/07/iStock-944453634-750x450.jpg"></img>
+      <div className={s.loginBlock}>
+        <NavLink to="/login">Login</NavLink>
+      </div>
     </header>
   );
 };
