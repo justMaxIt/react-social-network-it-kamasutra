7a3f56c04fde9d60da219f5d7c0064235fbc59f2
justMaxIt <maximfedor2015@gmail.com>
2020-12-31 17:56:43 +0200

    62 completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import s from "./Header.module.css";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={s.header}>
       <img src="https://blog.matcharesident.com/wp-content/uploads/2019/07/iStock-944453634-750x450.jpg"></img>
       <div className={s.loginBlock}>
-        <NavLink to="/login">Login</NavLink>
+        {props.isAuth ? props.login : <NavLink to="/login">Login</NavLink>}
       </div>
     </header>
   );
