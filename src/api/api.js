3955c62d3ed8749634dda2ef56ffe22db4910372
justMaxIt <maximfedor2015@gmail.com>
2021-01-04 23:22:46 +0200

    63 completed DAL add api component axios.create

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+  withCredentials: true,
+  headers: {
+    "API-KEY": "619b1ce3-a907-4252-bd76-a690bdde4068",
+  },
+  baseURL: `https://social-network.samuraijs.com/api/1.0/`,
+});
+
+export const usersAPI = {
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance
+      .get(`users?page=${currentPage}&count=${pageSize}`)
+      .then((response) => {
+        return response.data;
+      });
+  },
+};
