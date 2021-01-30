6b226dbb4ce36c5c122ec89230b77353225af447
justMaxIt <maximfedor2015@gmail.com>
2021-01-30 23:34:24 +0200

    71 ProfileStatus - class

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import s from "./ProfileInfo.module.css";
 
-const ProfileStatus = (props) => {
-  return (
-    <>
-      <div>
-        <span>{props.status}</span>
-      </div>
-      <div>
-        <input value={props.status}></input>
-      </div>
-    </>
-  );
-};
+class ProfileStatus extends React.Component {
+  render() {
+    return (
+      <>
+        <div>
+          <span>{this.props.status}</span>
+        </div>
+        <div>
+          <input value={this.props.status}></input>
+        </div>
+      </>
+    );
+  }
+}
 export default ProfileStatus;
