1958cfb99cf353477c1392d24cfb68bd0b41a0d6
justMaxIt <justmaksit@gmail.com>
2021-04-12 17:10:39 +0300

    75 implement Field and reduxForm

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,20 +1,29 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
+
+const LoginForm = (props) => {
+  return (<form onSubmit={props.handleSubmit}>
+  <div>
+    <Field placeholder={"Login"} name={"login"} component={"input"}/>
+  </div>
+  <div>
+    <Field placeholder={"Password"} name={"password"}  component={"input"} />
+  </div>
+  <div>
+    <Field component={"input"} name={"rememberMe"} type={"Checkbox"} />remember me
+  </div>
+  <button>Login</button>
+</form>)
+  };
+
+const LoginReduxForm = reduxForm({form: "login"})(LoginForm)
+
+
 
 const Login = (props) => {
   return <div>
 <h1>LOGIN</h1>
-<form>
-  <div>
-    <input placeholder={"Login"} />
-  </div>
-  <div>
-    <input placeholder={"Password"} />
-  </div>
-  <div>
-    <input type={"Checkbox"} />remember me
-  </div>
-  <button>Login</button>
-</form>
+<LoginReduxForm />
   </div>
   ;
 };
