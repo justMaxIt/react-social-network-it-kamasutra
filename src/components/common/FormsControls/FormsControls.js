fffe55810c38cf83d3d597fc71d4f394314d7142
justMaxIt <justmaksit@gmail.com>
2021-04-16 22:59:35 +0300

    77 added FormsControls.module.css to  FormsControl & check textarea for  error

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -1,10 +1,12 @@
 import React from "react"
+import s from "./FormsControls.module.css"
 
 export const Textarea = ({input, meta, ...props}) =>{
-
+const hasError = meta.touched && meta.error
    return (
-<div>
-<textarea {...input} {...props} />
+<div className={s.formControl + " " + (hasError? s.error: "")}>
+<div><textarea {...input} {...props} /></div>
+{hasError && <span>{meta.error}</span>}
 </div>
    )
 }
