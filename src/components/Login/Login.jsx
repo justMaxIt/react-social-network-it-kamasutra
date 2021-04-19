58da5775d2f5c2db5d2eafdfbda6dd134747897c
justMaxIt <justmaksit@gmail.com>
2021-04-19 13:16:26 +0300

    78 + login component is connected with API

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,19 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Field, reduxForm } from "redux-form";
 import { maxLengthCreator, required } from "../../utils/validators/validators";
 import { Input } from "../common/FormsControls/FormsControls";
+import {login} from "../../redux/authReducer"
 
-const maxLength10 = maxLengthCreator(10)
+const maxLength50 = maxLengthCreator(50)
 const LoginForm = (props) => {
   return (<form onSubmit={props.handleSubmit}>
      {/* e.preventDefault
      get all form data and put them to object
      props.onSubmit(formData) */}
   <div>
-    <Field placeholder={"Login"} name={"login"} component={Input} validate={[required, maxLength10]} />
+    <Field placeholder={"Email"} name={"email"} component={Input} validate={[required, maxLength50]} />
   </div>
   <div>
-    <Field placeholder={"Password"} name={"password"}  component={Input} validate={[required, maxLength10]} />
+    <Field placeholder={"Password"} name={"password"} type={"password"} component={Input} validate={[required, maxLength50]} />
   </div>
   <div>
     <Field component={Input} name={"rememberMe"} type={"Checkbox"} />remember me
@@ -28,7 +30,7 @@ const LoginReduxForm = reduxForm({form: "login"})(LoginForm)
 
 const Login = (props) => {
   const onSubmit = (formData)=>{
-console.log(formData);
+props.login(formData.email, formData.password, formData.rememberMe);
   }
   return <div>
 <h1>LOGIN</h1>
@@ -37,4 +39,4 @@ console.log(formData);
   ;
 };
 
-export default Login;
+export default connect(null, {login})(Login);
--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -13,7 +13,7 @@ const authReducer = (state = initialState, action) => {
     case SET_USER_DATA: {
       return {
         ...state,
-        ...action.data,
+        ...action.payload,
         isAuth: true,
       };
     }
@@ -23,16 +23,16 @@ const authReducer = (state = initialState, action) => {
   }
 };
 
-export const setAuthUserData = (userId, email, login) => ({
+export const setAuthUserData = (userId, email, login, isAuth) => ({
   type: SET_USER_DATA,
-  data: { userId, email, login },
+  payload: { userId, email, login, isAuth },
 });
 
 export const getAuthUserData = () => (dispatch) => {
   usersAPI.subscribeMe().then((data) => {
     if (data.resultCode === 0) {
       let { id, email, login } = data.data;
-      dispatch(setAuthUserData(id, email, login));
+      dispatch(setAuthUserData(id, email, login, true));
     }
   });
 };
@@ -41,6 +41,14 @@ export const login = (email, password, rememberMe) => (dispatch) => {
   authAPI.login(email, password, rememberMe).then((data) => {
     if (data.resultCode === 0) {
       dispatch(getAuthUserData());
+    }
+  });
+};
+
+export const logout = () => (dispatch) => {
+  authAPI.logout().then((data) => {
+    if (data.resultCode === 0) {
+      dispatch(setAuthUserData(null, null, null, false));
     }
   });
 };
