a4706bece8c8dbe67bf19527ecb902d5c9d5a252
justMaxIt <justmaksit@gmail.com>
2021-04-23 23:20:56 +0300

    79 completed

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -14,7 +14,7 @@ class ProfileContainer extends Component {
   componentDidMount() {
     let userId = this.props.match.params.userId;
     if (!userId) {
-      userId = 13779;
+      userId = this.props.authorizedUserId;
     }
     this.props.getUserProfile(userId);
     this.props.getStatus(userId);
@@ -27,6 +27,7 @@ class ProfileContainer extends Component {
           profile={this.props.profile}
           status={this.props.status}
           updateStatus={this.props.updateStatus}
+
         />
       </div>
     );
@@ -36,6 +37,8 @@ class ProfileContainer extends Component {
 let mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
   status: state.profilePage.status,
+  authorizedUserId: state.auth.userId,
+  isAuth: state.auth.isAuth
 });
 
 export default compose(
