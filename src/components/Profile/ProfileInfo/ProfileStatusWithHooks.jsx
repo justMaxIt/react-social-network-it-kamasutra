d04e47bef94bde15e5717d5c9da1f3683583959e
justMaxIt <justmaksit@gmail.com>
2021-05-01 00:21:05 +0300

    84 completed changed ProfileStatusWithHooks by adding useState

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -3,10 +3,18 @@ import React, { useState } from "react";
 
 const ProfileStatusWithHooks = (props) => {
   let [editMode, setEditMode] = useState(false)
+  let [status, setStatus] = useState(props.status)
 
-let activateEditMode = ()=> {
+const activateEditMode = ()=> {
   setEditMode(true)
 }
+const deActivateEditMode = () => {
+  setEditMode(false);
+  props.updateStatus(status);
+};
+const onStatusChange = (e) => {
+  setStatus(e.currentTarget.value);
+};
 
       return (
       <>
@@ -20,8 +28,10 @@ let activateEditMode = ()=> {
         {editMode && (
           <div>
             <input
-             
+              onChange={onStatusChange}
               autoFocus={true}
+              onBlur={deActivateEditMode}
+              value={status}
                          />
           </div>
         )}
