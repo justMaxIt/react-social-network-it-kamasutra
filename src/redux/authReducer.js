4d8447f3b54bcbc302d9d0d355d3f15775974711
justMaxIt <justmaksit@gmail.com>
2021-05-05 23:24:32 +0300

    90 refactor authReducer: async await instead of then

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -1,6 +1,6 @@
 import { stopSubmit } from "redux-form";
 import { authAPI } from "../api/api";
-const SET_USER_DATA = "SET_USER_DATA";
+const SET_USER_DATA = "samurai-network/auth/SET_USER_DATA";
 
 let initialState = {
   userId: null,
@@ -28,32 +28,29 @@ export const setAuthUserData = (userId, email, login, isAuth) => ({
   payload: { userId, email, login, isAuth },
 });
 
-export const getAuthUserData = () => (dispatch) => {
-  return authAPI.subscribeMe().then((data) => {
-    if (data.resultCode === 0) {
-      let { id, email, login } = data.data;
+export const getAuthUserData = () => async (dispatch) => {
+  let response = await authAPI.subscribeMe();
+    if (response.data.resultCode === 0) {
+      let { id, email, login } = response.data.data;
       dispatch(setAuthUserData(id, email, login, true));
     }
-  });
 };
 
-export const login = (email, password, rememberMe) => (dispatch) => {
-  authAPI.login(email, password, rememberMe).then((data) => {
-    if (data.resultCode === 0) {
+export const login = (email, password, rememberMe) => async (dispatch) => {
+ let response = await authAPI.login(email, password, rememberMe);
+     if (response.data.resultCode === 0) {
       dispatch(getAuthUserData());
     } else {
-      let message = data.messages.length > 0 ? data.messages[0] : "Some error"
+      let message = response.data.messages.length > 0 ? response.data.messages[0] : "Some error"
       dispatch(stopSubmit("login", {_error: message}));
     }
-  });
 };
 
-export const logout = () => (dispatch) => {
-  authAPI.logout().then((data) => {
-    if (data.resultCode === 0) {
+export const logout = () => async (dispatch) => {
+  let response = authAPI.logout();
+    if (response.data.resultCode === 0) {
       dispatch(setAuthUserData(null, null, null, false));
     }
-  });
 };
 
 export default authReducer;
