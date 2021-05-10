0b1aa8afebd431d3dde96a69849edb15c64c9f64
justMaxIt <justmaksit@gmail.com>
2021-05-10 23:59:25 +0300

    92 new test

--- src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { create } from "react-test-renderer";
+import ProfileStatus from "./ProfileStatus";
 
 describe("ProfileStatus component", () => {
-  test("it shows the expected text when clicked (testing the wrong way!)", () => {
-    const component = create(<Button text="SUBSCRIBE TO BASIC" />);
+  test("status from props should be in the state", () => {
+    const component = create(<ProfileStatus status="IT-kamasutra.com" />);
     const instance = component.getInstance();
-    expect(instance.state.text).toBe("");
+    expect(instance.state.status).toBe("IT-kamasutra.com");
   });
+  test("after creation span should be shown", () => {
+    const component = create(<ProfileStatus status="IT-kamasutra.com" />);
+    const root = component.root;
+    let span = root.findByType("span");
+    expect(span.length).toBe(1);
+  });
+
+
+
 });
